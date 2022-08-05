0533b23c8d403fe4824b630cbd5a175f10a7193c
Golffy183 <kowit_narongtuwapan@cmu.ac.th>
2022-08-05 19:35:56 +0700

    add move effect

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -10,6 +10,7 @@ namespace GameDev3.Project
     {
         [SerializeField]
         private float _gravity = 9.8f;
+        public PlayerControl playerControls;
 
         public CharacterController controller;
         public string player;
@@ -20,37 +21,54 @@ namespace GameDev3.Project
         public float turnSmoothTime = 0.1f;
         float turnSmoothVelocity;
 
+        private InputAction Horizontal1;
+        private InputAction Vertical1;
+        private InputAction Horizontal2;
+        private InputAction Vertical2;
+
         float horizontal1;
         float vertical1;
         float horizontal2;
         float vertical2;
 
-        public void OnHorizontal1(InputValue value)
+        public ParticleSystem dust;
+
+        private void Awake()
         {
-            horizontal1 = value.Get<float>();
+            playerControls = new PlayerControl();
         }
 
-        public void OnVertical1(InputValue value)
+        private void OnEnable()
         {
-            vertical1 = value.Get<float>();
+            Horizontal1 = playerControls.Gameplay.Horizontal1;
+            Horizontal1.Enable();
+            Vertical1 = playerControls.Gameplay.Vertical1;
+            Vertical1.Enable();
+            Horizontal2 = playerControls.Gameplay.Horizontal2;
+            Horizontal2.Enable();
+            Vertical2 = playerControls.Gameplay.Vertical2;
+            Vertical2.Enable();
         }
 
-        public void OnHorizontal2(InputValue value)
+        private void OnDisable()
         {
-            horizontal2 = value.Get<float>();
-        }
-
-        public void OnVertical2(InputValue value)
-        {
-            vertical2 = value.Get<float>();
+            Horizontal1.Disable();
+            Vertical1.Disable();
+            Horizontal2.Disable();
+            Vertical2.Disable();
         }
 
         void Update()
         {
+            horizontal1 = Horizontal1.ReadValue<float>();
+            vertical1 = Vertical1.ReadValue<float>();
+            horizontal2 = Horizontal2.ReadValue<float>();
+            vertical2 = Vertical2.ReadValue<float>();
+
             if (player == "Player1" && PauseMenu.GameIsPaused == false)
             {
                 Vector3 direction = new Vector3(horizontal1, 0f, vertical1).normalized;
-
+                Debug.Log(direction.magnitude);
                 if (direction.magnitude >= 0.1f)
                 {
                     float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
@@ -59,12 +77,12 @@ namespace GameDev3.Project
 
                     direction.y -= _gravity;
                     controller.Move(direction * speed * Time.deltaTime);
+                    CreateDust();
                 }
             }
             if (player == "Player2" && PauseMenu.GameIsPaused == false)
             {
                 Vector3 direction = new Vector3(horizontal2, 0f, vertical2).normalized;
-
                 if (direction.magnitude >= 0.1f)
                 {
                     float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
@@ -73,6 +91,7 @@ namespace GameDev3.Project
 
                     direction.y -= _gravity;
                     controller.Move(direction * speed * Time.deltaTime);
+                    CreateDust();
                 }
             }
             if (speed == 13 && speedTurnBack == false)
@@ -80,12 +99,15 @@ namespace GameDev3.Project
                 Invoke("defaultSpeed", 8);
                 speedTurnBack = true;
             }
-
         }
         void defaultSpeed()
         {
             speed = 10;
             speedTurnBack = false;
+        }
+        void CreateDust()
+        {
+            dust.Play();
         }
     }
 }
